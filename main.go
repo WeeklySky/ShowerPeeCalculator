5082cfb3e5a0e6078c55f2140889c270dc2f459c
icaro <icaro.isf@gmail.com>
2017-04-02 21:54:21 -0300

    Calculator

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main()  {
+	showerFlowMinute := flag.Float64("shower", 7.9, "Shower water flow in liters per minute.")
+
+	flushFlow := flag.Float64("flush", 6, "Water flow in liters in a single flush.")
+
+	flag.Parse()
+
+	showerFlowSeconds := *showerFlowMinute / 60;
+
+	fmt.Print("Enter how long did your pee last (in seconds): ")
+	var peeTime float64
+	fmt.Scanln(&peeTime)
+
+	waterSpent := peeTime * showerFlowSeconds;
+
+	if waterSpent < *flushFlow {
+		fmt.Printf("You saved %.2f liters of water by peeing in the shower. Good job I guess.\n", *flushFlow - waterSpent)
+	}
+
+	if waterSpent >= *flushFlow {
+		fmt.Println("You are wasting water!")
+		fmt.Printf("By peeing in the shower for longer than %.0f seconds, contrary to popular belief, you are actually wasting water.\n", *flushFlow / showerFlowSeconds)
+	}
+}
